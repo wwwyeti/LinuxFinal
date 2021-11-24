cf96a89bb21d67d44246bdd323048f7973c5b322
wwwyeti <1092478629@qq.com>
2021-11-24 19:16:34 +0800

    HelloworldJson

--- HelloworldJson.java
+++ HelloworldJson.java
@@ -5,17 +5,17 @@ import com.google.gson.*;
 
 public class HelloworldJson extends HttpServlet {
 
-	   public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		         response.setContentType("application/json;charset=UTF-8");
-         		       response.setCharacterEncoding("UTF-8");
-			             PrintWriter out = response.getWriter();
-				           Gson gson = new Gson();
-					         Student stu = new Student("yushurong",20);						
-						       String json = gson.toJson(stu);
-						             out.println(json);
-							           out.flush();
-								         out.close();
-									    }
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json;charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+		Gson gson = new Gson();
+		Student stu = new Student("yu",20);						
+		String json = gson.toJson(stu);
+		out.println(json);
+		out.flush();
+		out.close();
+	}
 }
 
 class Student{
