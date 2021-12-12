7ba58b5037ed2af739e6ab133ded989ccc98aab1
wwwyeti <1092478629@qq.com>
2021-12-12 16:30:31 +0800

    Book

---
+++ Book.java
@@ -0,0 +1,13 @@
+
+<ckage com.example;
+
+public class Book {
+    int id;
+    String name;
+    String author;
+
+    @Override
+    public String toString() {
+        return "Book [author=" + author + ", id=" + id + ", name=" + name + "]";
+    }
+}servlet>
